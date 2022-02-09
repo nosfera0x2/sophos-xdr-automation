83118838e9a5874867ddd1b6a441890e714dde80
spence-rat <82817752+spence-rat@users.noreply.github.com>
2022-02-09 17:01:26 -0600

    Update openAttachment.py
    -Define openEmailAttachment as function. 
    -Removed items that printed pywinauto outputs to console.

--- openAttachment.py
+++ openAttachment.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# Name:         
+# Name:         Open Email Attachment Function
 # Description:  Python Script to Automate XDR Threat Intel
 # Author:       Spencer Brown
 # URL:          
@@ -9,30 +9,35 @@
 from pywinauto import Application, Desktop
 import time
 
-#start the Outlook application
-app = Application(backend="uia").start(r'C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK')
-time.sleep(10)
-#lets see what windows are available, and assign it to a dialogue
-print(app.windows())
-#Main application is presented as 'Outlook Today - Outlook'
-#Define the main dialogue
-main_dlg = app['Outlook Today - Outlook']
-#To see if it works, lets see if we can print out the control identifiers.
-#main_dlg.print_control_identifiers()
-#success
-bth2_tree = main_dlg.child_window(title="BTH2", control_type="TreeItem")
-#bth2_tree.print_control_identifiers()
-treeItem_two = bth2_tree.child_window(title="2", control_type="TreeItem")
-treeItem_two.click_input(double=True)
-time.sleep(5)
-second_dlg = app['2 - Sophos - Outlook']
-time.sleep(5)
-#second_dlg.print_control_identifiers()
-email = second_dlg.child_window(title="With Attachments, Subject Your flight has been successfully booked!, Received Mon 1/31, Size 136 KB, Flag Status Unflagged, ", control_type="DataItem")
-email.click_input(double=True)
-time.sleep(5)
-print(app.windows())
-email_dlg = app['Your flight has been successfully booked! - Message (HTML) ']
-email_dlg.print_control_identifiers()
-attachment = email_dlg.child_window(title="Attachments", control_type="Pane")
-attachment.Button25.click_input(double=True)
+def openEmailAttachment():
+    #start the Outlook application
+    app = Application(backend="uia").start(r'C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK')
+    time.sleep(10)
+    #Main application is presented as 'Outlook Today - Outlook'
+    #Define the main dialogue
+    main_dlg = app['Outlook Today - Outlook']
+    #Define the bth2 tree item in the left pane of outlook
+    bth2_tree = main_dlg.child_window(title="BTH2", control_type="TreeItem")
+    #Define the '2' subfolder in the bth2 folder.
+    treeItem_two = bth2_tree.child_window(title="2", control_type="TreeItem")
+    #Presents the email available in the 2 subfolder
+    treeItem_two.click_input(double=True)
+    time.sleep(5)
+    #The title of the outlook application has now changed after selected the '2' subfolder
+    #Redefine the main dialogue, as 'second dialogue'
+    second_dlg = app['2 - Sophos - Outlook']
+    time.sleep(5)
+    #Define the email, and open it. 
+    email = second_dlg.child_window(title="With Attachments, Subject Your flight has been successfully booked!, Received Mon 1/31, Size 136 KB, Flag Status Unflagged, ", control_type="DataItem")
+    email.click_input(double=True)
+    time.sleep(5)
+     
+    #The email is now opened, and defined as an interactive dialogue 
+    #The attachment is identified and opened. 
+    email_dlg = app['Your flight has been successfully booked! - Message (HTML) ']
+    attachment = email_dlg.child_window(title="Attachments", control_type="Pane")
+    attachment.Button25.click_input(double=True)
+
+    
+if __name__ == '__main__':
+    openEmailAttachment()
