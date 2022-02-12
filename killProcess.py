50f061fc010376903f9be10ee5a369f1c3e667b9
spence-rat <82817752+spence-rat@users.noreply.github.com>
2022-02-12 13:46:03 -0500

    Update killProcess.py

--- killProcess.py
+++ killProcess.py
@@ -3,11 +3,13 @@ import wmi
 def killProcess(process):
     ti = 0
     name = process
+    print('The process I am seeking to kill is: ' + str(name))
     f = wmi.WMI()
 
     for process in f.Win32_Process():
         if process.name == name:
             process.Terminate()
+            print('I have found, and killed ' + str(name))
             ti +=1
     if ti == 0:
-        print('Process not found!')
+        print('There are no running instances of ' + str(name))
