0a2c71e8740902c41373cd44182e41908a3da1e9
spence-rat <82817752+spence-rat@users.noreply.github.com>
2022-02-12 13:45:31 -0500

    Update enableMacro.py

--- enableMacro.py
+++ enableMacro.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# Name:         Open Email Attachment Function
+# Name:         Enable Macro in Word Document Function
 # Description:  Python Script to Automate XDR Threat Intel
 # Author:       Spencer Brown
 # URL:          
@@ -8,43 +8,20 @@
 
 from pywinauto import Application, Desktop
 import time
-import psutil
-import os 
-import wmi 
 import win32gui
 import win32con
 from extractTitle import extractTitle
 
-
-def returnMicrosoftPID():
-    process_name = "WINWORD.EXE"
-    processID = None
-    for proc in psutil.process_iter():
-        if process_name in proc.name():
-            wordPID = proc.pid
-            return (wordPID)
-
-microsoftWord = returnMicrosoftPID()
-
-def openMacro():
+def enableMacro():
+    #Connects to open microsoft word winow using regex, since window name is dynamic.
     app = Application(backend="uia").connect(title_re='.*DeltaFlightItinerary')
-    #print(app.windows())
-    window = app.windows()
-    wordWindow = str(extractTitle(window[0]))
-    print(wordWindow)
-    type(wordWindow)
+    window = app.windows() #app.windows() returns a list object.
+    wordWindow = str(extractTitle(window[0])) #Extract title from list, and convert to string
     print("I'm looking for the window..." + wordWindow)
-    guiWindow = win32gui.FindWindow(None,wordWindow)
-    win32gui.ShowWindow(guiWindow,win32con.SW_SHOWMAXIMIZED)
-    #time.sleep(5)
-    print(app.windows())
+    guiWindow = win32gui.FindWindow(None,wordWindow) #Find our window with win32gui
+    win32gui.ShowWindow(guiWindow,win32con.SW_SHOWMAXIMIZED) #Maximize the window. 
     main_dlg = app[wordWindow]
-    main_dlg.set_focus()
-    main_dlg.print_control_identifiers()
-    main_dlg.EnableEditingButton.click_input() #enable editing, pivots to enable content
-    time.sleep(5)
-    #print('this is the second set of identifiers:')
-    #main_dlg.print_control_identifiers()
-    main_dlg.EnableContentButton.click_input() #enable content button
-    
-openMacro()
+    main_dlg.set_focus() #Make window visible for clicking inputs. 
+    main_dlg.EnableEditingButton.click_input() #Enable editing, pivots to enable content
+    time.sleep(3)
+    main_dlg.EnableContentButton.click_input() #Enable content button
