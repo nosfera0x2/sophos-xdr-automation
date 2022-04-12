6b5e99f2670cadadccbbb9ca62c7d0e2408de60b
spence-rat <82817752+spence-rat@users.noreply.github.com>
2022-04-12 15:24:38 -0500

    Delete returnPID.py

--- returnPID.py
+++
@@ -1,9 +0,0 @@
-import psutil
-
-def returnPID(process):
-    process_name = process
-    processID = None
-    for proc in psutil.process_iter():
-        if process_name in proc.name():
-            processID = proc.pid
-            return (processID)
