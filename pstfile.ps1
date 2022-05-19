0d2a94e2c8683f0a97fd662254d0634d45371718
spence-rat <82817752+spence-rat@users.noreply.github.com>
2022-05-18 20:48:53 -0500

    Delete pstfile.ps1

--- pstfile.ps1
+++
@@ -1,9 +0,0 @@
-$PSTPath = "C:\threat"
-Add-type -assembly "Microsoft.Office.Interop.Outlook"
-$outlook = new-object -comobject outlook.application
-$namespace = $outlook.GetNameSpace("MAPI")
-
-Get-ChildItem $PSTPath -Filter *.pst |
-ForEach-Object {
-    $namespace.AddStore($_.FullName)
-}
