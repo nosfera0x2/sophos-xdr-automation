63343355040b4b39555b814519aa67569ac72efa
spence-rat <82817752+spence-rat@users.noreply.github.com>
2022-05-18 22:06:01 -0500

    Update delta.py
    Connecting to application by PATH.
    Added revert_tree function

--- delta.py
+++ delta.py
@@ -2,26 +2,26 @@ import pywinauto
 import time 
 import sys
 
-outlook_start = 'C:\\Program Files (x86)\\Microsoft Office\\root\\Office16\\OUTLOOK.EXE'
+outlook_path = 'C:\\Program Files (x86)\\Microsoft Office\\root\\Office16\\OUTLOOK.EXE'
 outlook_connect = "Outlook Today - Outlook"
 
 
 class pywinApp:
 
-    def __init__(self,start=None,connect=None):
-        self.start = start
-        self.connect = connect
+    def __init__(self,path):
+        self.path = path
+       
 
     def startapp(self):
-        app = pywinauto.Application(backend="uia").start(self.start)
+        app = pywinauto.Application(backend="uia").start(self.path)
         return app
 
     def connectapp(self):  
-        app = pywinauto.Application(backend="uia").connect(title_re=self.connect)
+        app = pywinauto.Application(backend="uia").connect(path=self.path,visible_only=False)
         return app
     
 
-microsoft_outlook = pywinApp(start=outlook_start,connect=outlook_connect)
+microsoft_outlook = pywinApp(outlook_path)
 
 outlook_app = microsoft_outlook.connectapp()
 outlook=outlook_app['Outlook Today - Outlook']
@@ -37,10 +37,10 @@ def check_window(filepath,string,dialogue_object):
     with open(filepath, 'r') as file:
         content = file.read()
         if string in content:
-            print('Correct Dialogue Found!')
+            print(f'Dialogue: {string} in {dialogue_object} located.')
             return True
         else:
-            print('Dialogue does not exist')
+            print(f'Dialogue: {string} in {dialogue_object} not found.')
             return False
         
 wizard = outlook['Microsoft Office Activation Wizard']
@@ -49,3 +49,22 @@ if check_window(filepath,the_wizard,outlook) == True:
     wizard.child_window(title="Close", control_type="Button").click_input()
 else:
     print("This didn't work!")
+
+outlook.child_window(title="Sophos", control_type="TreeItem").click_input(double=True)
+sophos = outlook.child_window(title="Sophos", control_type="TreeItem")
+
+
+def revert_tree():
+    global filepath
+    global sophos
+    if check_window(filepath,"Atomic Red Team",sophos) == True:
+        sophos.click_input(double=True)
+        print("Tree is reset")
+    else:
+        print("oops")
+        pass
+
+
+
+
+
